53b94385b193c1125e0e645b7915c35785763339
Francis McConville <francis.mcconville@axomic.com>
2024-06-28 16:02:02 +0100

    rough db session manager for postgres

---
+++ src/database/main.py
@@ -0,0 +1,38 @@
+import logging
+import sys
+from contextlib import contextmanager
+from os import environ
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+log = logging.getLogger()
+
+
+def _get_db_url() -> str:
+    username = environ.get("DB_USER")
+    password = environ.get("DB_PASS")
+    host = environ.get("DB_HOST")
+    port = environ.get("DB_PORT")
+    db_name = environ.get("DB_NAME")
+
+    # Breaking dependency inversion because I only plan on using postgres
+    # Could probably make this respect DIP by not hardcoding the driver
+    return f"postgresql://{username}:{password}@{host}:{port}/{db_name}"
+
+
+try:
+    engine = create_engine(_get_db_url())
+    session_factory = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+except:  # noqa
+    log.critical("Failed to connect to database")
+    sys.exit(1)
+
+
+@contextmanager
+def session(**kwargs) -> Session:
+    _session = session_factory(**kwargs)
+    try:
+        yield _session
+    finally:
+        _session.close()
